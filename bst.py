dfa68e2d14e985a1c55f0b4c93f546b788b3b290
Gourav Lokhande <46524941+07gaura@users.noreply.github.com>
2022-01-29 21:16:04 +0530

    Update bst.py

--- bst.py
+++ bst.py
@@ -29,14 +29,48 @@ def postOrder(root):
     postOrder(root.left)
     postOrder(root.right)
     print(root.val)
+
+def delete(root,data):
+    if root is None:
+        return root
+    elif root.val<data:
+        root.right = delete(root.right,data)
+    elif root.val>data:
+        root.left = delete(root.left,data)
+    else:
+        if root.left is None and root.right is None:
+            return None
+        if root.left is None:
+            temp = root.right
+            root = None
+            return temp
+        elif root.right is None:
+            temp = root.left
+            root = None
+            return temp
+        temp = getSuccessor(root)
+        root.val=temp.val
+        root.right = delete(root.right,temp.val)
+    return root
         
-r = Node(50)
-r = insert(r, 30)
-r = insert(r, 20)
-r = insert(r, 40)
-r = insert(r, 70)
-r = insert(r, 60)
-r = insert(r, 80)
- 
+def getSuccessor(root):
+    if root is None:
+        return None
+    temp = root.right
+    while temp.left is not None:
+        temp = temp.left
+    return temp
+    
+        
+root = None
+root = insert(root, 50)
+root = insert(root, 30)
+root = insert(root, 20)
+root = insert(root, 40)
+root = insert(root, 70)
+root = insert(root, 60)
+root = insert(root, 80)
+root = delete(root, 20)
+root = delete(root, 50)
 # Print inoder traversal of the BST
-inOrder(r)
+inOrder(root)
